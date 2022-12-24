ec59b66d28abacbd927ce6015ce4f80e8d876573
FikremariamMolla <ce.fikre.mariam@gmail.com>
2022-12-24 10:46:16 -0800

    counter updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import './style.css';
 
+import { React, useState } from 'react'
+ 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+ 
+  //increase counter
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  //decrease counter
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+ 
+  //reset counter 
+  const reset = () =>{
+    setCounter(0)
+  }
+ 
   return (
-    <div>
-      <h1>This is fikre, study web</h1>
-      <p>l love to code each day.</p>
+    <div className="counter">
+      <h1>React Counter</h1>
+      <span className="counter__output">{counter}</span>
+      <div className="btn__container">
+        <button className="control__btn" onClick={increase}>+</button>
+        <button className="control__btn" onClick={decrease}>-</button>
+        <button className="reset" onClick={reset}>Reset</button>
+      </div>
     </div>
   );
 }
